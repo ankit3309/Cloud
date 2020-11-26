17728a85652103f5ea53950c86d1d03388a892b1
Ankit Barwal <barwalankit12345@gmail.com>
2020-11-26 16:44:06 +0545

    feat(ci): run tests on pre-push

---
+++ cypress/integration/examples/TestCase.js
@@ -0,0 +1,12 @@
+
+describe('TestSuite', function()
+{
+    it('Verify Title of the Page', function()
+    {
+        cy.visit('https://www.lftechnology.com/')
+        cy.title().should('eq', 'Leapfrog Technology')    
+    })
+
+
+
+})
